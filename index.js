fe70b386a7d91c9377f022f938bc0c9a4ff00a3c
Jonathan Harrell <harr041@gmail.com>
2020-02-29 20:31:50 -0600

    update

--- index.js
+++ index.js
@@ -3,34 +3,38 @@ const path = require('path')
 const fs = require('fs')
 
 const fetchImage = url => {
-	return fs.readFileSync(path.join(__dirname, '../..', `/public${url}`), 'utf8')
+  return fs.readFileSync(path.join(__dirname, '../..', `/public${url}`), 'utf8')
 }
 
 module.exports = ({ markdownAST }, pluginOptions) => {
-	visit(markdownAST, 'image', (node) => {
-		const { url, alt, title } = node
+  visit(markdownAST, 'image', (node) => {
+    const { url, alt, title } = node
 
-		if (url.endsWith('.svg')) {
-			let html = fetchImage(url)
+    if (url.endsWith('.svg')) {
+      let html = fetchImage(url)
 
-			if (pluginOptions.colors) {
-				Object.entries(pluginOptions.colors).forEach(([color, replacement]) => {
-					const re = new RegExp(color, "g")
-					html = html.replace(re, replacement)
-				})
-			}
+      if (pluginOptions.colors) {
+        Object.entries(pluginOptions.colors).forEach(([color, replacement]) => {
+          const re = new RegExp(color, 'g')
+          html = html.replace(re, replacement)
+        })
+      }
 
-			if (alt) html = html.replace(/<title>.*<\/title>/, `<title>${alt}</title>`);
+      const svgAttrsRe = new RegExp(/<svg(.*)>/)
+      const [, svgAttrs] = html.match(svgAttrsRe)
+      if (alt) html = html
+        .replace(/<title>.*<\/title>/, `<title>${alt}</title>`)
+        .replace(svgAttrs, `${svgAttrs} role="img"`)
 
-			if (html) {
-				node.type = 'html'
-				node.value = `<figure>
-					${html.trim()}
-					${title ? `<figcaption>${title}</figcaption>` : ''}
-				</figure>`
-			}
-		}
-	})
+      if (html) {
+        node.type = 'html'
+        node.value = `<figure>
+          ${html.trim()}
+          ${title ? `<figcaption>${title}</figcaption>` : ''}
+        </figure>`
+      }
+    }
+  })
 
-	return markdownAST
+  return markdownAST
 }
